6bcad091485e16b166a4c54151e8543a4c91d00b
jc-773 <jonc960@gmail.com>
2024-04-05 15:05:17 -0400

    remove redis service calls

--- src/main/java/com/project/sbws/backend/controllers/nba/PlayerStatsBySeason.java
+++ src/main/java/com/project/sbws/backend/controllers/nba/PlayerStatsBySeason.java
@@ -1,6 +1,5 @@
 package com.project.sbws.backend.controllers.nba;
 
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.sbws.backend.responses.NBADotComPlayerStatsRowSet;
 import com.project.sbws.backend.responses.PlayerStatsNBADotCom;
 import com.project.sbws.backend.services.implementation.NBAPlayerStatsService;
-import com.project.sbws.backend.services.implementation.redis.RedisPlayerStatsBySeasonService;
 import com.project.sbws.backend.services.interfaces.IBackendRequestService;
 
 
@@ -23,22 +21,18 @@ public class PlayerStatsBySeason {
     
     IBackendRequestService backendRequestService;
     NBAPlayerStatsService playerStatsService;
-    private RedisPlayerStatsBySeasonService redisPlayerStatsBySeasonService;
    
 
     @Autowired
-    public PlayerStatsBySeason(IBackendRequestService backendRequestService, RedisPlayerStatsBySeasonService redisPlayerStatsBySeasonService) {
+    public PlayerStatsBySeason(IBackendRequestService backendRequestService) {
         this.backendRequestService = backendRequestService;
         this.playerStatsService = new NBAPlayerStatsService();
-        this.redisPlayerStatsBySeasonService = redisPlayerStatsBySeasonService;
     }
 
     @RequestMapping(value = "/nba/player/stats/byseason", method=RequestMethod.GET)
      public ResponseEntity <List<NBADotComPlayerStatsRowSet>> getPlayerStatsBySeason(@RequestHeader(value = "playerID", required = false)String playerID) {
         try {
                 Map<String, PlayerStatsNBADotCom> playerStats = backendRequestService.PlayerCareerStats(playerID);
-                List<NBADotComPlayerStatsRowSet> basePlayerDashboardByYear = playerStatsService.filterPlayerStatsMapToBasePlayerDashboard(playerStats);
-                redisPlayerStatsBySeasonService.savePlayerStatsBySeason(playerID, null);
                 return playerStatsService.getPlayerCareerStats(playerID, playerStats);
 
         } catch (Exception e) {
